d4798920e8f94413000043ea2b9c236226e25743
labore <akodjolabore@gmail.com>
2023-01-19 18:36:49 +0000

    add button

---
+++ front-end/constants/index.js
@@ -0,0 +1,4 @@
+export const NFT_CONTRACT_ABI = "abi-of-your-nft-contract";
+export const NFT_CONTRACT_ADDRESS = "address-of-your-nft-contract";
+export const TOKEN_CONTRACT_ABI = "abi-of-your-token-contract";
+export const TOKEN_CONTRACT_ADDRESS = "address-of-your-token-contract";
